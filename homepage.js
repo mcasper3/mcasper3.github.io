e3cd73fe03c842bd20a450cc2ad8f721528bff0a
Mike <mikecasper395@gmail.com>
2015-05-30 11:00:22 -0500

    updated looks of the home page

---
+++ homepage.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $("#github")
+        .mouseover(function () {
+            $(this).attr("src", "GitHub-Mark-Highlighted.png");
+        })
+        .mouseout(function () {
+            $(this).attr("src", "GitHub-Mark.png");
+        });
+});
