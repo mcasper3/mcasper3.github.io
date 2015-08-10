972afa9be5e40e20731dcebf3edf74ce260eda16
Mike <mikecasper395@gmail.com>
2015-08-10 18:30:19 -0500

    fixed issue with tabs

--- AboutMe/aboutme.js
+++ AboutMe/aboutme.js
@@ -1,5 +1,1 @@
-﻿$(document).ready(function () {
-    $("#headerContent").load("/SiteFeatures/header.html", function () {
-        $("#aboutMeLink").attr("id", "currentTab");
-    });
-});
+﻿
--- homepage.js
+++ homepage.js
@@ -1,6 +1,1 @@
-﻿$(document).ready(function () {
-    $("#headerContent").load("SiteFeatures/header.html", function () {
-        $("#homeLink").attr("id", "currentTab");
-
-    });
-});
+﻿
